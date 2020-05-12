88246de12cf599e0a799f1295f5d9e4b81c73854
isikf <62076587+isikf@users.noreply.github.com>
2020-05-12 17:24:53 +0100

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -3,5 +3,7 @@ package day_1;
 public class Login {
     public static void main(String[] args) {
         //slack users can login successfully
+        
+        //Brand new changes to be applied in IntelliJ"
     }
 }
